a56fb3af0055192fe0853d78e3f3be2e7283f022
Rohin <me@r0h.in>
2020-05-26 12:03:56 -0400

    Authored by John Liu (@WolfyWolf33): Allow ppl who aren't signed in to access list view

--- js/list.js
+++ js/list.js
@@ -26,64 +26,102 @@ function loadlist() {
         }
         
         $.ajax(settings).done(function (response) {
-            db.collection('users').doc(user.uid).get().then(function(doc) {
-                userpinned = doc.data().favs
-                if (userpinned == undefined || userpinned == null) {
-                    userpinned = []
-                }
-                for (let i = 0; i < response.response.length; i++) {
-                    const element = response.response[i];
-            
-                    a = document.createElement('a')
-                    a.classList.add('list-group-item')
-                    a.classList.add('flex-column')
-                    a.classList.add('animated')
-                    a.classList.add('fadeInUp')
-                    a.classList.add('align-items-start')
-            
-                    if (element.deaths.new == null) {
-                        element.deaths.new = '+0'
-                    }
-                    if (element.cases.new == null) {
-                        element.cases.new = '+0'
-                    }
-                    
-                    ispinned = false
-                    for (let i = 0; i < userpinned.length; i++) {
-                        if (userpinned[i] == element.country) {
-                            ispinned = true
-                            document.getElementById('pinnedtext').style.display = 'block'
-                        }
-                    }
 
-                    if (ispinned) {
-                        favFunc = "unstar('" + element.country + "', this)"
-                    }
-                    else {
-                        favFunc = "star('" + element.country + "', this)"
-                    }
-
-                    isCountry = false
-                    for (let i = 0; i < aresponse.response.length; i++) {
-                        if (aresponse.response[i] == element.country) {
-                            isCountry = true
+            if (user.uid == undefined || user.uid == null) {
+                    for (let i = 0; i < response.response.length; i++) {
+                        const element = response.response[i];
+                
+                        a = document.createElement('a')
+                        a.classList.add('list-group-item')
+                        a.classList.add('flex-column')
+                        a.classList.add('animated')
+                        a.classList.add('fadeInUp')
+                        a.classList.add('align-items-start')
+                
+                        if (element.deaths.new == null) {
+                            element.deaths.new = '+0'
                         }
-                    }
-        
-                    if (isCountry) {
-                        if (ispinned) {
-                            a.innerHTML = '<div class="d-flex w-100 justify-content-between">         <h5 class="mb-1"><b>' + element.country + '</b></h5>   <small>Updated on ' + element.day + '.</small>       </div>       <center>           <br>       <div class="row">           <div style="color: var(--list-item-one)" class="col-sm">               <h3 class="leadnum">' + element.cases.active + '</h3>               <p>Active Cases</p><h4>' + element.cases.new + '</h4>           </div>           <div style="color: lime;" class="col-sm">             <h3 class="leadnum">' + element.cases.recovered + '</h3>              <p>Recovered</p>      <button onclick="' + favFunc + '" class="eon-text"><i class="material-icons">star</i></button>    </div>         <div style="color: rgb(255, 96, 96);" class="col-sm">             <h3 class="leadnum">' + element.deaths.total + '</h3>   <p>Deaths</p> <h4>' + element.deaths.new + '</h4></div> </div></center>'
-                            document.getElementById('pinnedlist').appendChild(a)
-                        }
-                        else {
-                            a.innerHTML = '<div class="d-flex w-100 justify-content-between">         <h5 class="mb-1"><b>' + element.country + '</b></h5>   <small>Updated on ' + element.day + '.</small>       </div>       <center>           <br>       <div class="row">           <div style="color: var(--list-item-one)" class="col-sm">               <h3 class="leadnum">' + element.cases.active + '</h3>               <p>Active Cases</p><h4>' + element.cases.new + '</h4>           </div>           <div style="color: lime;" class="col-sm">             <h3 class="leadnum">' + element.cases.recovered + '</h3>              <p>Recovered</p>      <button onclick="' + favFunc + '" class="eon-text"><i class="material-icons">star_border</i></button>    </div>         <div style="color: rgb(255, 96, 96);" class="col-sm">             <h3 class="leadnum">' + element.deaths.total + '</h3>   <p>Deaths</p> <h4>' + element.deaths.new + '</h4></div> </div></center>'
-                            document.getElementById('regionlist').appendChild(a)
+                        if (element.cases.new == null) {
+                            element.cases.new = '+0'
                         }
                         
+                            favFunc = "star('" + element.country + "', this)"
+    
+                        isCountry = false
+                        for (let i = 0; i < aresponse.response.length; i++) {
+                            if (aresponse.response[i] == element.country) {
+                                isCountry = true
+                            }
+                        }
+            
+                        if (isCountry) {
+                            a.innerHTML = '<div class="d-flex w-100 justify-content-between">         <h5 class="mb-1"><b>' + element.country + '</b></h5>   <small>Updated on ' + element.day + '.</small>       </div>       <center>           <br>       <div class="row">           <div style="color: var(--list-item-one)" class="col-sm">               <h3 class="leadnum">' + element.cases.active + '</h3>               <p>Active Cases</p><h4>' + element.cases.new + '</h4>           </div>           <div style="color: lime;" class="col-sm">             <h3 class="leadnum">' + element.cases.recovered + '</h3>              <p>Recovered</p>      <button onclick="' + favFunc + '" class="eon-text"><i class="material-icons">star_border</i></button>    </div>         <div style="color: rgb(255, 96, 96);" class="col-sm">             <h3 class="leadnum">' + element.deaths.total + '</h3>   <p>Deaths</p> <h4>' + element.deaths.new + '</h4></div> </div></center>'
+                            document.getElementById('regionlist').appendChild(a)
+                            
+                        }
                     }
-                }
-                addWaves()
-            })
+                    addWaves()
+            }
+            else {
+                db.collection('users').doc(user.uid).get().then(function(doc) {
+                    userpinned = doc.data().favs
+                    if (userpinned == undefined || userpinned == null) {
+                        userpinned = []
+                    }
+                    for (let i = 0; i < response.response.length; i++) {
+                        const element = response.response[i];
+                
+                        a = document.createElement('a')
+                        a.classList.add('list-group-item')
+                        a.classList.add('flex-column')
+                        a.classList.add('animated')
+                        a.classList.add('fadeInUp')
+                        a.classList.add('align-items-start')
+                
+                        if (element.deaths.new == null) {
+                            element.deaths.new = '+0'
+                        }
+                        if (element.cases.new == null) {
+                            element.cases.new = '+0'
+                        }
+                        
+                        ispinned = false
+                        for (let i = 0; i < userpinned.length; i++) {
+                            if (userpinned[i] == element.country) {
+                                ispinned = true
+                                document.getElementById('pinnedtext').style.display = 'block'
+                            }
+                        }
+    
+                        if (ispinned) {
+                            favFunc = "unstar('" + element.country + "', this)"
+                        }
+                        else {
+                            favFunc = "star('" + element.country + "', this)"
+                        }
+    
+                        isCountry = false
+                        for (let i = 0; i < aresponse.response.length; i++) {
+                            if (aresponse.response[i] == element.country) {
+                                isCountry = true
+                            }
+                        }
+            
+                        if (isCountry) {
+                            if (ispinned) {
+                                a.innerHTML = '<div class="d-flex w-100 justify-content-between">         <h5 class="mb-1"><b>' + element.country + '</b></h5>   <small>Updated on ' + element.day + '.</small>       </div>       <center>           <br>       <div class="row">           <div style="color: var(--list-item-one)" class="col-sm">               <h3 class="leadnum">' + element.cases.active + '</h3>               <p>Active Cases</p><h4>' + element.cases.new + '</h4>           </div>           <div style="color: lime;" class="col-sm">             <h3 class="leadnum">' + element.cases.recovered + '</h3>              <p>Recovered</p>      <button onclick="' + favFunc + '" class="eon-text"><i class="material-icons">star</i></button>    </div>         <div style="color: rgb(255, 96, 96);" class="col-sm">             <h3 class="leadnum">' + element.deaths.total + '</h3>   <p>Deaths</p> <h4>' + element.deaths.new + '</h4></div> </div></center>'
+                                document.getElementById('pinnedlist').appendChild(a)
+                            }
+                            else {
+                                a.innerHTML = '<div class="d-flex w-100 justify-content-between">         <h5 class="mb-1"><b>' + element.country + '</b></h5>   <small>Updated on ' + element.day + '.</small>       </div>       <center>           <br>       <div class="row">           <div style="color: var(--list-item-one)" class="col-sm">               <h3 class="leadnum">' + element.cases.active + '</h3>               <p>Active Cases</p><h4>' + element.cases.new + '</h4>           </div>           <div style="color: lime;" class="col-sm">             <h3 class="leadnum">' + element.cases.recovered + '</h3>              <p>Recovered</p>      <button onclick="' + favFunc + '" class="eon-text"><i class="material-icons">star_border</i></button>    </div>         <div style="color: rgb(255, 96, 96);" class="col-sm">             <h3 class="leadnum">' + element.deaths.total + '</h3>   <p>Deaths</p> <h4>' + element.deaths.new + '</h4></div> </div></center>'
+                                document.getElementById('regionlist').appendChild(a)
+                            }
+                            
+                        }
+                    }
+                    addWaves()
+                })
+            }
         });
     });
 }
